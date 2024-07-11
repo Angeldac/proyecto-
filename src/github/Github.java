058a22f6f3c3527f90f4949044f5d7630621f8a7
Angel Alejandro <angel.daniel.alejandro@outlook.com>
2024-07-11 12:00:39 -0600

    no se que poner

---
+++ src/github/Github.java
@@ -0,0 +1,16 @@
+
+package github;
+
+
+public class Github {
+
+    
+    public static void main(String[] args) {
+        int valor1=10;
+        for (int i = 1; i <= 10; i++) {
+            valor1=10*i;
+            System.out.println("10"+"*"+i+"="+valor1);   
+        }
+    }
+    
+}
